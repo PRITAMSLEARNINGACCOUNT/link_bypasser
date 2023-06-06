49efaa2e6267aecd6226c96e86c274e7609cddfe
pritamstech <98208329+PRITAMSLEARNINGACCOUNT@users.noreply.github.com>
2023-06-06 21:28:44 +0530

    Update LINK_BYPASSER.py

--- LINK_BYPASSER.py
+++ LINK_BYPASSER.py
@@ -2,7 +2,8 @@ import PyBypass
 print("This programme can bypass shortner links and new gdtot links")
 url = input("Enter Your URL???\n")
 hello = url.split("/")
-if hello[2] == "new7.gdtot.cfd":
+final_split = hello[2].split(".")
+if final_split[1] == "gdtot":
     bypassed_link = PyBypass.bypass(
         f"{url}", gdtot_crypt="dlNPbnNRd3hVUHNqTzFUQ0puSDRnd1FhRXNGUVZ6ZlpYejVMdmY1ZTRrST0%3D")
     print(f"This is your bypassed link = {bypassed_link}")
@@ -12,3 +13,4 @@ else:
         print(f"This is your bypassed link = {bypassed_link}")
     except Exception as e:
         print(e)
+
